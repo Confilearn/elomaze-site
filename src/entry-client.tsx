7e0f47be51285e34562a4a2d5832302bd5fc7267
Confilearn <confilearn@gmail.com>
2026-04-15 09:28:31 +0100

    fix: add missing index file, installed nito plugin for vercel deployment

---
+++ src/entry-client.tsx
@@ -0,0 +1,10 @@
+import { hydrateRoot } from "react-dom/client";
+import { RouterProvider } from "@tanstack/react-router";
+import { getRouter } from "./router";
+
+const router = getRouter();
+
+hydrateRoot(
+  document.getElementById("root")!,
+  <RouterProvider router={router} />,
+);
