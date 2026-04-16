0753c2866f1c096dc230b216c4774ab3cdbcae0f
Confilearn <confilearn@gmail.com>
2026-04-16 02:57:04 +0100

    debug

--- api/server.js
+++ api/server.js
@@ -1,4 +1,27 @@
-// Placeholder - will be replaced during build
-export default async function handler(req, res) {
-  res.status(200).send('Building...');
+import handler from "../dist/server/server.js";
+
+export default async function (req, res) {
+  try {
+    const url = `https://${req.headers.host}${req.url}`;
+
+    const request = new Request(url, {
+      method: req.method,
+      headers: req.headers,
+      body: req.method !== "GET" && req.method !== "HEAD" ? req : undefined,
+    });
+
+    const response = await handler(request);
+
+    res.status(response.status);
+
+    response.headers.forEach((value, key) => {
+      res.setHeader(key, value);
+    });
+
+    const text = await response.text();
+    res.send(text);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
 }
